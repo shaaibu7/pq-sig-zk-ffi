072ad14def40e1e6f8964f2dba23978997d1e144
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-02-03 20:58:03 +0100

    chore: clean key gen and pq sig ops

--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,17 @@ package main
 
 void gen_keypair(
     uint64_t seed,
+    uint32_t activation_epoch
+);
+
+void message_signing(
+    uint64_t seed,
+    uint32_t activation_epoch,
+    uint32_t signing_epoch
+);
+
+void sig_verification(
+    uint64_t seed,
     uint32_t activation_epoch,
     uint32_t signing_epoch
 );
@@ -16,6 +27,17 @@ import "fmt"
 
 func main() {
     C.gen_keypair(
+        C.uint64_t(1),
+        C.uint32_t(5),
+    )
+    
+    C.message_signing(
+        C.uint64_t(1),
+        C.uint32_t(5),
+        C.uint32_t(4),
+    )
+    
+    C.sig_verification(
         C.uint64_t(1),
         C.uint32_t(5),
         C.uint32_t(4),
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,16 +6,75 @@ use rand::{SeedableRng, rngs::StdRng};
 #[unsafe(no_mangle)]
 pub extern "C" fn gen_keypair(
     seed: u64,
-    activation_epoch: u32,
-    signing_epoch: u32
+    activation_epoch: u32
 ) {
 
     type LeanSignatureScheme = leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
     /// The public key type from the leansig library.
     type LeanSigPublicKey = <LeanSignatureScheme as SignatureScheme>::PublicKey;
     
-    /// The secret key type from the leansig library.
-    type LeanSigSecretKey = <LeanSignatureScheme as SignatureScheme>::SecretKey;
+    let mut rng = StdRng::seed_from_u64(seed);
+    let lifetime = 1 << 3;
+    
+
+    let (pk, sk) = LeanSignatureScheme::key_gen(&mut rng, activation_epoch as usize, lifetime);
+
+    let pk_byte = pk.to_bytes();
+    let sk_bytes = sk.to_bytes();
+    
+    let _ = pk_byte;
+    let _ = sk_bytes;
+  
+    let pub_key = LeanSigPublicKey::from_bytes(&pk_byte).unwrap();
+    // let secret_key = LeanSigSecretKey::from_bytes(&sk_bytes).unwrap();
+
+    println!("============================key generation completed==================================");
+    println!("The public key data is: root {:?} parameter {:?}", pub_key.root(), pub_key.parameter())
+    
+    
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn message_signing(
+    seed: u64,
+    activation_epoch: u32,
+    signing_epoch: u32
+) {
+
+    type LeanSignatureScheme = leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
+    
+    /// The signature type from the leansig library.
+    type LeanSigSignature = <LeanSignatureScheme as SignatureScheme>::Signature;
+
+    let mut rng = StdRng::seed_from_u64(seed);
+    let lifetime = 1 << 3;
+    let message: [u8; 32] = [0; 32];
+
+    let (_, sk) = LeanSignatureScheme::key_gen(&mut rng, activation_epoch as usize, lifetime);
+
+    let sig = LeanSignatureScheme::sign(&sk, signing_epoch, &message).unwrap();
+    
+
+    let sig_bytes = sig.to_bytes();
+    
+    
+    let signature = LeanSigSignature::from_bytes(&sig_bytes).unwrap();
+
+    println!("============================message signing completed==================================");
+    println!("The byte representation of the data is: {:?}", sig_bytes);
+    
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn sig_verification(
+    seed: u64,
+    activation_epoch: u32,
+    signing_epoch: u32
+) {
+
+    type LeanSignatureScheme = leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
+    
 
     let mut rng = StdRng::seed_from_u64(seed);
     let lifetime = 1 << 3;
@@ -35,13 +94,7 @@ pub extern "C" fn gen_keypair(
     let _ = pk_byte;
     let _ = sk_bytes;
     let _ = sig_bytes;
-    
-    let pub_key = LeanSigPublicKey::from_bytes(&pk_byte).unwrap();
-    let secret_key = LeanSigSecretKey::from_bytes(&sk_bytes).unwrap();
 
-    
+    println!("============================signature verification completed==================================");
     println!("The signature validity status is: {}", is_valid_sig);
-    println!("The public key data is: root {:?} parameter {:?}", pub_key.root(), pub_key.parameter())
-    
-    
 }
